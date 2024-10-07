3d08bb9f5f1206db6ebdee374aabdc9971522d15
Happiste <jonathan.bitane@gmail.com>
2024-10-07 20:01:56 +0300

    Create exo.py

---
+++ exo.py
@@ -0,0 +1,80 @@
+# Excercice 1 
+first = "Hello World"
+
+#Excercice 2 
+# This is a comment 
+
+#Excercice 3
+print("I AM A COMPUTER")
+
+#Excercice 4
+if 1 < 2 and 4 > 2:
+    print("Math is fun.")
+    
+#Excercice 5
+nope = None 
+
+#Excercice 6
+result = True and False
+print(result)
+
+#Excercice 7
+string = "What's my Length?"
+print(len(string))
+
+#excercice 8 
+shouting = "i am shouting"
+shouting_upper = shouting.upper()
+print(shouting_upper)
+
+#excercice 9
+num_str = "123"
+type(num_str)
+print(type(num_str))
+num = int(num_str)
+print(type(num))
+
+#Excercice 10 
+num = 4
+word = "real"
+for_real = str(num) + word
+print(for_real)
+
+#Excercice 11
+brooklyn = 3 * "cool"
+print(brooklyn)
+
+#Excercçce 12
+#ZeroDivisionError: Division by Zero 
+
+#Excercice 13 
+print(type([]))
+
+#Excercice 14
+name = input("what is your name >> ")
+print("Your name is " + name )
+
+#Excercice 15
+num = input("Please, chose a number >> ")
+real_num = int(num)
+if real_num < 0:
+    print("That number is less than 0")
+elif real_num > 0: 
+    print("That number is greater than 0")
+else:
+    print("you picked 0")
+    
+#Excercice 16
+fruit = "apple"
+index_fruit = word.index('l')
+print(index_fruit)
+
+#Excercice 17
+instrument = "xylophone"
+is_a_y = "y" in instrument
+print(is_a_y)
+
+#Excercice 18
+string = "my_string"
+my_string = string.islower()
+print(my_string)
