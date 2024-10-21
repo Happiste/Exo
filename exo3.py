acbd2878841668962be66274bb005e45b7da59ad
Happiste <jonathan.bitane@gmail.com>
2024-10-21 17:36:00 +0300

    Update exo3.py

--- exo3.py
+++ exo3.py
@@ -1,27 +1,30 @@
-list_two = [("name", "Tim"), ("job", "doctor"), ("age", "30")]
-result = dict(list_two)
-print(result)
-
-lsit_one = [("name", "Tim"), ("job", "doctor"), ("age", "30")]
-result = {x: value for x, value in lsit_one}
-print(result)
-
-list1 = ["CA", "NJ", "RI"]
-list2 = ["California", "New Jersey", "Rhode Island"]
-result = {key: value for key, value in zip(list1, list2)}
-#result = list(zip(list1, list2))
-print(result)
-
-vowels = 'aeiou'
-dico = {}
-for v in vowels:
-    dico[v] = 0
+#exo1
+lst = [("name", "Elie"), ("job", "Instructor")]
+dico = {x : val for x,  val in lst}
 print(dico)
 
-alpha = {i + 1: chr(i + 65) for i in range(26)}
-print(alpha)
+#exo2
+lst1 = ["CA", "NJ", "RI"]
+lst2 = ["California", "New Jersey", "Rhode Island"]
+zipped = zip(lst1, lst2)
+print(type(zipped))
+zipped2 = list(zipped)
+print(zipped2)
+dico = {x : val for x, val in zipped2}
+print(dico)
 
-str = "jonathan bitane"
+#exo3
 vowels = 'aeiou'
-dico = {key: str.count(key) for key in vowels}
+dico = {x : 0 for x in vowels}
+print (dico)
+
+#exo4
+val = chr(65)
+dico = {x + 1 : chr(x + 65) for x in range(0,26)}
+print(dico)
+
+#exo5 
+salsa = "awesome sauce"
+vowels = "aeiou"
+dico  = {key : salsa.count(key) for key, in vowels}
 print(dico)
