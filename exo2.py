63b5dd8bcd4d86a3996108a481d9da846d4028d8
Happiste <jonathan.bitane@gmail.com>
2024-10-21 16:45:29 +0300

    Update exo2.py

--- exo2.py
+++ exo2.py
@@ -1,62 +1,55 @@
-#Excercice 1 
-my_list = [1, 2, 3, 4]
-for num in my_list:
-    print(num)
-    
-#Excercice 2
-my_list = [1, 2, 3, 4]
-for num in my_list:
-    num = num*20
-    print(num)    
-    
-#Excercice 3
+#exo 1
+lst = [1, 2, 3, 4]
+print(lst)
+
+#exo 2
+new_lst = [x * 20 for x in lst]
+print(new_lst)
+
+#exo 3
 name_list = ["Elie", "Tim", "Matt"]
-capitalize = [new_list[0] for new_list  in name_list]
-print(capitalize)
+first_letter_list = [x[0] for x in name_list]
+print(first_letter_list)
 
-#Excercice 4
-my_list = [1,2,3,4,5,6]
-new_list = [num for num in my_list if num % 2 == 0]
+#exo4
+lst = [1, 2, 3, 4, 5, 6]
+even_lst = [x for x in lst if x % 2 == 0]
+print(even_lst)
 
-print(new_list)
+#exo5
+lst1 = [1,2,3,4]
+lst2 = [3,4,5,6]
+lst3 = [x for x in lst1 if x in lst2]
+print(lst3)
 
-#Excercice 5
-first_list = [1,2,3,4]
-second_list = [3,4,5,6]
-final_list = [num for num in first_list if num in second_list]
-print(final_list)
-
-#Excercice 6
-name_list = ["Elie", "Tim", "Matt"]
-reverse_list = [name[::-1].lower() for name in name_list]
+#exo 6
+reverse_list = [x[::-1].lower() for x in name_list]
 print(reverse_list)
 
-#Excercice 7 
-word_first = "first"
-word_third = "third"
-final = [num for num in word_first if num in word_third]
-print(final)
+#exo 7
+first = "first"
+third = "third"
+lst = [x for x in first if x in third]
+print(lst)
 
-#Excercice 8
-number_list = list(range(1, 101))
-new_list = [num for num in number_list if num % 12 == 0]
-print(new_list)
+#exo 8 
+num = range(1,101)
+list_num = list(num)
+divideby12 = [x for x in list_num if x % 12 == 0]
+print(divideby12)
 
-#Excercice 9 
-consonant = []
-for letters in 'amazing':
-    if letters in ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']:
-        consonant.append(letters)
+#exo 9
+vowels = "aeiou"
+str1 = "amazing"
+lst = [x for x in str1 if x not in vowels]
+print(lst)
 
-print(consonant)
-    
-#Excercice 10 
-list_matr = [[0, 1, 2] for num in range (3)]
+#exo 10
+lst = [[0,1,2] for x in range(1,4)]
+print(lst)
 
-print(list_matr)
-
-#Excercice 11
-list_matr = [[i for i in range(10)] for num in range(10)]
-print(list_matr)
-
+#exo 11
+lst = list(range(1,10))
+matrix = [lst for x in range(1, 10)]
+print(matrix)
 
